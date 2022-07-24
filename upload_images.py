2dfc6d36788b23c69330306f9c51ae8609e721cc
Riley Thompson <rileythompson99@gmail.com>
2022-07-24 18:47:48 -0400

    prints

--- upload_images.py
+++ upload_images.py
@@ -5,6 +5,8 @@ if __name__ == '__main__':
     f = open('images.csv')
     rows = csv.reader(f)
     for row in rows:
+        print(f'adding {row[0]} {row[1]}')
         db.add_image(row[0], row[1])
+        print(f'added {row[0]} {row[1]}')
     db.close()
 
