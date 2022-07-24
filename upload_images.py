bbbf738ce3da5ba3d06bf9aa13806ca61e943840
Riley Thompson <rileythompson99@gmail.com>
2022-07-24 18:50:41 -0400

    fix

--- upload_images.py
+++ upload_images.py
@@ -1,12 +1,9 @@
 if __name__ == '__main__':
     import csv
     from db import NbaDB
-    db = NbaDB
+    db = NbaDB()
     f = open('images.csv')
     rows = csv.reader(f)
     for row in rows:
-        print(f'adding {row[0]} {row[1]}')
         db.add_image(row[0], row[1])
-        print(f'added {row[0]} {row[1]}')
     db.close()
-
