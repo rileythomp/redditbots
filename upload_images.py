aa884fd4e7f4f725107d10729fa7b3e8dd8db333
Riley Thompson <rileythompson99@gmail.com>
2022-07-24 18:45:04 -0400

    upload images

---
+++ upload_images.py
@@ -0,0 +1,10 @@
+if __name__ == '__main__':
+    import csv
+    from db import NbaDB
+    db = NbaDB
+    f = open('images.csv')
+    rows = csv.reader(f)
+    for row in rows:
+        db.add_image(row[0], row[1])
+    db.close()
+
