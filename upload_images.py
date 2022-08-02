7336c9e6d52dc710ccc3db28619b2c9779cbb51c
Riley Thompson <rileythompson99@gmail.com>
2022-08-01 20:58:11 -0400

    upload images

--- upload_images.py
+++ upload_images.py
@@ -4,6 +4,13 @@ if __name__ == '__main__':
     db = NbaDB()
     f = open('images.csv')
     rows = csv.reader(f)
+    i = 0
     for row in rows:
-        db.add_image(row[0], row[1])
+        i += 1
+        if i > 614:
+            db.add_image(row[0], f'https://nbapics.s3.amazonaws.com/{"-".join(row[0].split())}.svg')
+        elif row[1] != '':
+            db.add_image(row[0], f'https://nbapics.s3.amazonaws.com/{"-".join(row[0].split())}.png')
+        else:
+            db.add_image(row[0], row[1])
     db.close()
