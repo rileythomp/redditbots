c3c89497fc0402d52fb70147ad8b2e08f1795d04
Riley Thompson <riley.thompson@flybits.com>
2022-11-19 11:54:40 -0500

    attempt to improve performance

---
+++ create_indexes.sql
@@ -0,0 +1,6 @@
+CREATE INDEX IF NOT EXISTS mentions_mention_type_index ON mentions (mention_type);
+CREATE INDEX IF NOT EXISTS nba_comments_timestamp_index ON nba_comments (timestamp);
+CREATE INDEX IF NOT EXISTS nba_comments_comment_id_index ON nba_comments (comment_id);
+CREATE INDEX IF NOT EXISTS mentions_comment_id_index ON mentions (comment_id);
+CREATE INDEX IF NOT EXISTS mentions_name_index ON mentions (name);
+CREATE INDEX IF NOT EXISTS names_name_index ON names (name);
